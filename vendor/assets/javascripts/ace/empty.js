750902bca146630f42d0e195bc4ce1229c74ac91
Roberto Bartolome <roberto.bartolome.medina@gmail.com>
2013-06-28 15:33:23 +0200

    wip

---
+++ vendor/assets/javascripts/ace/empty.js
@@ -0,0 +1,6 @@
+//// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Add page specific .js files to a directory named for this page
+//
+//= require_directory ./empty
